6438b92ca35bf534e706a398106b5b7fd790e5f2
axhello <axuan460079@163.com>
2025-03-24 10:46:55 +0800

    message6: Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
+from selenium.webdriver.common.by import By
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         #return super().setUp()
@@ -11,14 +14,23 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         #检查标题是否有如下的内容
         self.assertIn('To-Do',self.browser.title,f"browser title was: {self.browser.title}")
-        self.fail('Finish the test!')
+        #检查头部包含'To-Do'
+        header_text=self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',header_text)
 
         #应用中有文本输入框
+        inputbox=self.browser.find_element(By.ID,'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+        
         #输入"Buy flowers"
-
-        #页面更新
-        #代办列表中新增1. Buy flowers
-
+        inputbox.send_keys('Buy flowers')
+        #回车后页面更新
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        #待办列表中新增1. Buy flowers
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1. Buy flowers',[row.text for row in rows])
         #继续输入文本"Send a gift to Lisi"
 
         #页面更新
@@ -28,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
 
         #访问URL，发现待办事项列表仍在
         #离开
-
+        self.fail('Finish the test!')
 
 
 if __name__=='__main__':
