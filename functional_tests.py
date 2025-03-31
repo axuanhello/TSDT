770a8475b22d6a2cc359a3f046839d570aeffe72
axhello <axuan460079@163.com>
2025-03-31 10:40:20 +0800

    message9: After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser=webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text,[row.text for row in rows])
     def test_case(self):
         #打开网页
         self.browser.get('http://localhost:8000')
@@ -26,23 +30,19 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Buy flowers')
         #回车后页面更新
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
         #待办列表中新增1. Buy flowers
-        table=self.browser.find_element(By.ID,'id_list_table')
-        rows=table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1. Buy flowers',[row.text for row in rows])
+        self.check_for_row_in_list_table('1. Buy flowers')
+        
         #继续输入文本"Give a gift to Lisi"
         inputbox=self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
         #页面更新
         #新增列表
-        table=self.browser.find_element(By.ID,'id_list_table')
-        rows=table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1. Buy flowers',[row.text for row in rows])
-        self.assertIn('2. Give a gift to Lisi',[row.text for row in rows])
-        
+        self.check_for_row_in_list_table('1. Buy flowers')
+        self.check_for_row_in_list_table('2. Give a gift to Lisi')
         #网站为待办生成唯一的URL
 
         #访问URL，发现待办事项列表仍在
