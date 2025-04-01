6382901a37a5bff53027e2cb66fa3817881a5344
axhello <axuan460079@163.com>
2025-04-01 21:35:27 +0800

    message11: Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -29,6 +29,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response=self.client.get('/')
+        self.assertIn('itemey 1',response.content.decode())
+        self.assertIn('itemey 2',response.content.decode())
     # def test_home_page_return_correct_html(self):
     #     request=HttpRequest()
     #     response=home_page(request)
@@ -41,5 +47,16 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response,'home.html')
     def test_can_save_a_POST_request(self):
         response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
-        self.assertTemplateUsed(response,'home.html')
+        #检查数据库中是否存储
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+    def test_redirects_after_POST(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        #self.assertIn('A new list item',response.content.decode())
+        #self.assertTemplateUsed(response,'home.html')
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
